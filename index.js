c718fabcfee1dca110ee51fa6b476e234d01cf2a
djoseph1250 <djoseph1250@gmail.com>
2017-03-21 18:03:02 -0400

    Added Cross Domain functionality

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ var mongoose = require("mongoose");
 var restful = require('node-restful');
 var appPort = (process.env.PORT || 8000);
 var connectionString = process.env.MONGODB_URI;
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+};
 var app = express();
+app.use(allowCrossDomain);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
