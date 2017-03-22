69047d0cf292e87dda1aefda35a46b9aa165c4a9
djoseph1250 <djoseph1250@gmail.com>
2017-03-21 23:51:15 -0400

    removed incrementing id

--- models/people.js
+++ models/people.js
@@ -1,33 +1,10 @@
 "use strict";
 var mongoose = require("mongoose");
-var autoIncrement = require("mongoose-auto-increment");
-
-var connection = mongoose.createConnection(process.env.MONGODB_URI);
-autoIncrement.initialize(connection);
-
+;
 var peopleSchema = new mongoose.Schema({
     name: String,
     favoriteCity: String
 });
-
-peopleSchema.plugin(autoIncrement.plugin, {
-    model: 'Person',
-    startAt: 1,
-    incrementBy: 1
-});
-var Person = connection.model("Person", peopleSchema);
-var person = new Person();
-
-
-person.save(function (err) {
-    // person._id === 100 -> true
-    person.nextCount(function(err, count) {
-        // count === 101 -> true
-        person.resetCount(function(err, nextCount) {
-            // nextCount === 100 -> true
-        });
-    });
-});
-
+var Person = mongoose.model("Person", peopleSchema);
 module.exports = Person;
 //# sourceMappingURL=people.js.map
