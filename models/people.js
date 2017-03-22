256073ea7b8406d6bd1898bbec3af1a6d1d8d264
djoseph1250 <djoseph1250@gmail.com>
2017-03-21 22:22:27 -0400

    added incrementing ID

--- models/people.js
+++ models/people.js
@@ -1,10 +1,16 @@
 "use strict";
 var mongoose = require("mongoose");
-;
+var autoIncrement = require("mongoose-auto-increment");
+
+var connection = mongoose.createConnection(process.env.MONGODB_URI);
+autoIncrement.initialize(connection);
+
 var peopleSchema = new mongoose.Schema({
     name: String,
     favoriteCity: String
 });
-var Person = mongoose.model("Person", peopleSchema);
+
+peopleSchema.plugin(autoIncrement.plugin, "Person");
+var Person = connection.model("Person", peopleSchema);
 module.exports = Person;
 //# sourceMappingURL=people.js.map
